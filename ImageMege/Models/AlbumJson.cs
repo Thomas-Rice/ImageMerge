e8d7b8a7f28a1c7549d82d820ab6e3f8271636d0
Thomas-Rice <thomas.rice@asos.com>
2018-07-28 19:32:34 +0100

    test

--- ImageMege/Models/AlbumJson.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ImageMege.Models
-{
-    public class AlbumJson
-    {
-        public int userId { get; set; }
-        public int id { get; set; }
-        public string title { get; set; }
-    }
-}
